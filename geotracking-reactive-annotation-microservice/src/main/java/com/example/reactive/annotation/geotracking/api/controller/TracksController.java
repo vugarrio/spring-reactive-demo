d3eb4c44089d2c757a399871f9067d35c489df07
Vicente Ugarrio <vugarrio@minsait.com>
2023-04-12 02:01:26 +0200

    Edit api definition

--- geotracking-reactive-annotation-microservice/src/main/java/com/example/reactive/annotation/geotracking/api/controller/TracksController.java
+++ geotracking-reactive-annotation-microservice/src/main/java/com/example/reactive/annotation/geotracking/api/controller/TracksController.java
@@ -1,5 +1,7 @@
 package com.example.reactive.annotation.geotracking.api.controller;
 
+import com.example.reactive.annotation.geotracking.dto.TrackResponseDTO;
+import com.example.reactive.annotation.geotracking.dto.UserGeoPointResponseDTO;
 import com.example.reactive.annotation.geotracking.service.GeoPointService;
 import com.example.reactive.annotation.geotracking.api.TracksApi;
 import com.example.reactive.annotation.geotracking.dto.TrackDTO;
@@ -9,6 +11,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.time.OffsetDateTime;
 
 
 @RestController
@@ -23,5 +27,21 @@ public class  TracksController implements TracksApi {
     @Override
     public Mono<ResponseEntity<TrackRefDTO>> processTrack(TrackDTO trackDTO, ServerWebExchange exchange) {
         return geoPointService.processTrack(trackDTO).map(ResponseEntity::ok);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Mono<ResponseEntity<UserGeoPointResponseDTO>> getLastPosition(String user, String deviceId, ServerWebExchange exchange) {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Mono<ResponseEntity<TrackResponseDTO>> getTrackByParams(String user, OffsetDateTime dateFrom, OffsetDateTime dateTo, ServerWebExchange exchange) {
+        return null;
     }
 }
